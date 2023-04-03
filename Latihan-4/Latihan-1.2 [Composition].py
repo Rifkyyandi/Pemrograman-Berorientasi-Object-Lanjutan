ebbaee7d31d1a577429da0adcb0a94a5cfa4e7c2
Rifkyyandi <122662185+Rifkyyandi@users.noreply.github.com>
2023-04-03 21:44:36 +0700

    Add files via upload

---
+++ Latihan-4/Latihan-1.2 [Composition].py
@@ -0,0 +1,26 @@
+class Menu:
+    def __init__(self, dishes=None):
+        if dishes is None:
+            self.dishes = []
+        else:
+            self.dishes = dishes
+    
+    def add_dish(self, dish):
+        self.dishes.append(dish)
+
+class Dish:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+class Restaurant:
+    def __init__(self, name, menu):
+        self.name = name
+        self.menu = menu
+
+dish1 = Dish("Nasi Goreng", 15000)
+dish2 = Dish("Mie Goreng", 12000)
+
+menu = Menu([dish1, dish2])
+restaurant = Restaurant("Warung Padang", menu)
+restaurant.menu.dishes # output: [dish1, dish2]
---
+++ Latihan-4/Latihan-1.4 [Composition].py
@@ -0,0 +1,18 @@
+class RAM:
+    def __init__(self, capacity):
+        self.capacity = capacity
+
+class Storage:
+    def __init__(self, capacity):
+        self.capacity = capacity
+
+class Computer:
+    def __init__(self, ram, storage):
+        self.ram = ram
+        self.storage = storage
+
+ram = RAM(8)
+storage = Storage(500)
+computer = Computer(ram, storage)
+print(computer.ram.capacity) # output: 8
+print(computer.storage.capacity) # output: 500
